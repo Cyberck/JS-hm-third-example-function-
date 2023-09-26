2765382a5d61198c174a356337740bc1d38c78f3
unknown <Baxtiyorddushaev@gmail.com>
2023-09-27 01:11:21 +0500

    second

--- script-3.js
+++ script-3.js
@@ -4,15 +4,15 @@
 // Ovyavleniye koda proishodit pri pomoshi kluchevogo slovo function
 // primer sozdaniye funksii ' function imya funcsii () {} '
 
-// let number1 = +prompt('chislo 1');
-// let number2 = +prompt('chislo 2');
+let number1 = +prompt('chislo 1');
+let number2 = +prompt('chislo 2');
 
-//  function sum(num1, num2) {
-//    let result = num1 + num2 * 3;
-//    return result; // tut nado govarit vozvrashay resultat
-//  }
-//  console.log (sum(number1, number2));//eto argumenti. Esli ne peredat parametri on budet raven NaN
-//  // prioritet argumenta vishe budet
+ function sum(num1, num2) {
+   let result = num1 + num2 * 3;
+   return result; // tut nado govarit vozvrashay resultat
+ }
+ console.log (sum(number1, number2));//eto argumenti. Esli ne peredat parametri on budet raven NaN
+ // prioritet argumenta vishe budet
 
 
 
